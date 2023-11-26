c9fc3704d2fe3bb05e971967eaf858fd6a24addb
Azure0413 <88129659+Azure0413@users.noreply.github.com>
2023-11-26 11:34:08 +0800

    Add files via upload

---
+++ 消費者/frontend/src/component/timepicker.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+
+const TimePicker = () => {
+  const currentHour = new Date().getHours();
+  const [selectedHour, setSelectedHour] = useState(currentHour);
+  const [selectedMinute, setSelectedMinute] = useState(10);
+
+  useEffect(() => {
+    // You can use selectedHour and selectedMinute here as needed
+    console.log(`Selected time: ${selectedHour}:${selectedMinute}`);
+  }, [selectedHour, selectedMinute]);
+
+  const availableHours = Array.from({ length: 21 - currentHour }, (_, index) => currentHour + index);
+  const availableMinutes = [10, 20, 30, 40, 50];
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <div style={{ marginRight: '10px' }}>
+        <label htmlFor="hour"></label>
+        <select
+          id="hour"
+          value={selectedHour}
+          onChange={(e) => setSelectedHour(parseInt(e.target.value))}
+          style={{width:'100px',marginLeft:"60px",border:"2px solid gray"}}
+        >
+          {availableHours.map((hour) => (
+            <option key={hour} value={hour}>
+              {hour}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div>
+        <label htmlFor="minute"></label>
+        <select
+          id="minute"
+          value={selectedMinute}
+          onChange={(e) => setSelectedMinute(parseInt(e.target.value))}
+          style={{width:'100px',marginLeft:"10px",border:"2px solid gray"}}
+        >
+          {availableMinutes.map((minute) => (
+            <option key={minute} value={minute}>
+              {minute}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default TimePicker;
