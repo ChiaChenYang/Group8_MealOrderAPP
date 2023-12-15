5787c22ebf454ab1b2a938f971a48873b76667c1
YuHsuanHu <138638366+YuHsuanHu@users.noreply.github.com>
2023-12-15 16:57:40 +0800

    修改取得菜單的 api

--- services/ConsumerMenuService.js
+++ services/ConsumerMenuService.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const asyncHandler = require("express-async-handler");
-const { menus, restaurants,  menucategories, menuitems, tags, itemtags, itemmenucategories, orders, Sequelize } = require('../models');
+const { menus, restaurants,  restaurantlatestnews, menucategories, menuitems, tags, itemtags, itemmenucategories, orders, Sequelize } = require('../models');
 
 exports.getSingleItem = asyncHandler(async (itemId) => {
   try {
@@ -30,17 +30,13 @@ exports.getSingleItem = asyncHandler(async (itemId) => {
       ],
     });
     const formattedItem = {
-      restaurantId: SingleItemDetails.restaurantId,
-      itemId: SingleItemDetails.menuitems[0].itemId,
-      itemName: SingleItemDetails.menuitems[0].itemName,
-      itemImage: SingleItemDetails.menuitems[0].itemImage.toString('base64'),
-      descriptionText: SingleItemDetails.menuitems[0].descriptionText,
-      price: SingleItemDetails.menuitems[0].price,
-      calories: SingleItemDetails.menuitems[0].calories,
-      tags: SingleItemDetails.menuitems[0].tags.map((tag) => ({
-        tagId: tag.tagId,
-        tagName: tag.tagName,
-      })),
+      shop_id: SingleItemDetails.restaurantId,
+      name: SingleItemDetails.menuitems[0].itemName,
+      price: SingleItemDetails.menuitems[0].price,    
+      Calorie: SingleItemDetails.menuitems[0].calories,
+      tag: SingleItemDetails.menuitems[0].tags.map((tag) => tag.tagName),
+      description: SingleItemDetails.menuitems[0].descriptionText,
+      image: item.itemImage ? item.itemImage.toString('base64') : null,
     };
     return formattedItem;
   } catch (error) {
@@ -48,6 +44,171 @@ exports.getSingleItem = asyncHandler(async (itemId) => {
   }
 });
 
+exports.getAllMenuDetailsForConsumer = asyncHandler(async (restaurantId) => {
+  try {
+    // 檢查該餐廳 id 是否存在餐廳表
+    // const restaurant = await restaurants.findByPk(restaurantId);
+    const restaurant = await restaurants.findByPk(restaurantId, {
+      include: [
+          {
+              model: restaurantlatestnews,
+              attributes: ['newsContent']
+          }
+      ]
+  });
+    if (!restaurant.restaurantId) {
+      throw new Error(`Restaurant not found with ID: ${restaurantId}`);
+    }
+
+    let prepareTime = null;
+    let rating = null;
+    let commentsCount = null;
+    let stationStartDate = null;
+    let stationEndDate = null;
+
+    // 定義 currentDate
+    const currentDate = new Date();
+    currentDate.setHours(currentDate.getHours() + 8);
+
+    if (restaurant.istemporaryRestaurant) {
+      // 如果是輪動櫃，並且有有效的 stationStartDate 和 stationEndDate
+      stationStartDate = restaurant.stationStartDate;
+      stationEndDate = restaurant.stationEndDate;
+
+      const stationStartDateObj = new Date(stationStartDate);
+      const stationEndDateObj = new Date(stationEndDate);
+
+      console.log(stationStartDateObj, stationEndDateObj, currentDate);
+      
+      if (currentDate >= stationStartDateObj && currentDate <= stationEndDateObj) {
+        // 現在的日期在 stationStartDate 和 stationEndDate 之間
+        prepareTime = restaurant.prepareTime;
+
+        // 計算歷史評價
+        const historyRating = await orders.findAll({
+          attributes: [
+            [Sequelize.fn('AVG', Sequelize.col('orderRating')), 'avgRating'],
+            [Sequelize.fn('COUNT', Sequelize.col('orderRating')), 'commentsCount'],
+          ],
+          where: {
+            restaurantId: restaurantId,
+            completeTime: {
+              [Sequelize.Op.lte]: currentDate,
+            },
+          },
+        });
+
+        rating = historyRating[0].dataValues.avgRating || 0;
+        commentsCount = historyRating[0].dataValues.commentsCount || 0;
+        // 四捨五入到小數點第二位
+        rating = parseFloat(rating.toFixed(2));
+      }
+    } else {
+      // 如果不是輪動櫃，直接使用餐廳的 prepareTime
+      // 計算歷史評價
+      const historyRating = await orders.findAll({
+        attributes: [
+          [Sequelize.fn('AVG', Sequelize.col('orderRating')), 'avgRating'],
+          [Sequelize.fn('COUNT', Sequelize.col('orderRating')), 'commentsCount'],
+        ],
+        where: {
+          restaurantId: restaurantId,
+          completeTime: {
+            [Sequelize.Op.lte]: currentDate,
+          },
+        },
+      });
+      rating = historyRating[0].dataValues.avgRating || 0;
+      commentsCount = historyRating[0].dataValues.commentsCount || 0;
+      // 四捨五入到小數點第二位
+      rating = parseFloat(rating.toFixed(2));
+      prepareTime = restaurant.prepareTime;
+    }
+
+    const menuDetails = await menus.findAll({
+      where: { restaurantId },
+      attributes: ['menuId', 'menuName', 'menuTime', 'menuType'],
+      include: [
+        {
+          model: restaurants,
+        },
+        {
+          model: menucategories,
+          attributes: ['menuCategoryId', 'menuCategoryName'],
+          include: [
+            {
+              model: menuitems,
+              attributes: ['itemId', 'itemName', 'itemImage', 'descriptionText', 'price', 'calories', 'isAvailable'],
+              where: { isAvailable: true },
+              through: { attributes: [] },
+              include: [
+                {
+                  model: tags,
+                  attributes: ['tagId', 'tagName'],
+                  through: { attributes: [] },
+                },
+              ],
+            },
+          ],
+        }
+      ],
+    });
+
+    const formattedMenus = {
+      restaurantId: restaurant.restaurantId,
+      restaurantName: restaurant.restaurantName,
+      evaluate: rating,
+      comment: commentsCount,
+      prepare_time: prepareTime,
+      location: restaurant.factoryArea + restaurant.factoryLocation + restaurant.restaurantLocation,
+      stationStartDate: stationStartDate,
+      stationEndDate: stationEndDate,
+      news: restaurant.restaurantlatestnews.map(news => news.newsContent),
+      // menus: []
+    };
+    
+    menuDetails.forEach((menu) => {
+      const menuObj = {
+        menuId: menu.menuId,
+        type: [], // 新增一個 type 陣列
+      };
+    
+      menu.menucategories.forEach((menucategory, categoryIndex) => {
+        const items = {};
+    
+        menucategory.menuitems.forEach((item, itemIndex) => {
+          const tags = item.tags.map((tag) => tag.tagName);
+    
+          items[itemIndex + 1] = {
+            id: item.itemId,
+            name: item.itemName,
+            price: item.price,
+            Calorie: item.calories,
+            tag: tags,
+            description: item.descriptionText,
+            image: item.itemImage ? item.itemImage.toString('base64') : null,
+          };
+        });
+    
+        menuObj.type.push(menucategory.menuCategoryName);
+        menuObj[categoryIndex + 1] = items;
+      });
+      // 使用 menu.menuName 當 key
+      // formattedMenus.push({
+      //   [menu.menuName]: menuObj
+      // });
+      if (menuObj.type.length > 0) {
+        formattedMenus[menu.menuName] = menuObj;
+      }
+    });
+    return formattedMenus;
+    
+  } catch (error) {
+    throw new Error(`Error getting all menu details: ${error.message}`);
+  }
+});
+
+/*
 exports.getAllMenuDetailsForConsumer = asyncHandler(async (restaurantId) => {
   try {
     // 檢查該餐廳 id 是否存在餐廳表
@@ -201,4 +362,4 @@ exports.getAllMenuDetailsForConsumer = asyncHandler(async (restaurantId) => {
     throw new Error(`Error getting all menu details: ${error.message}`);
   }
 });
-
+*/
