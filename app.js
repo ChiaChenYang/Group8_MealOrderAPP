efb0c7a53c317887e1ff8111c97f438371b98cf9
Edward7820 <edward910530@gmail.com>
2023-12-16 11:24:28 +0800

    allow cors, fix spelling error in OrderService

--- app.js
+++ app.js
@@ -20,10 +20,11 @@ const db = require('./models');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
+  cors: {
+    origin: '*'
+  },
   connectionStateRecovery: {}
 });
-
-app.use(express.json({ limit: '50mb' }));  // 設置請求體大小限制為 50MB
 
 db.sequelize.sync({ alter: false });
 
@@ -32,7 +33,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
