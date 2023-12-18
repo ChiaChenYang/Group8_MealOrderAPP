2fdf95c1f282250a25420bcf555797fcfbcb7350
Azure0413 <88129659+Azure0413@users.noreply.github.com>
2023-12-18 18:10:42 +0800

    Add files via upload

---
+++ frontend_user/src/Main.js
@@ -0,0 +1,312 @@
+import React, { useState, useEffect } from "react";
+import ControllableStates from "./component/location";
+import image from "./image/main.png";
+import Navbar from "./component/navbar-component";
+import StarIcon from "@mui/icons-material/Star";
+import { Link, useParams } from "react-router-dom";
+// import imagei from "./image/imagei.jpg";
+import Selector from "./component/class";
+import Search from "./component/search";
+import CabinetDisplay from "./component/cabinet-component";
+import Notify from "./component/messenge-component";
+import io from "socket.io-client";
+
+function Main() {
+  const [selectedClass, setSelectedClass] = useState("全類別");
+  const [selectedLocation, setSelectedLocation] = useState("晶圓二廠");
+  const [searchResults, setSearchResults] = useState([]);
+  const { userId } = useParams();
+  const [load, setLoad] = useState(false);
+  const [restaurant_information, setRestaurants] = useState({});
+  const [error, setError] = useState(null);
+  console.log(error);
+  // const [m, setM] = useState(null);
+
+  // useEffect(() => {
+  //   const user_id = 2;
+
+  //   const socket = io();
+
+  //   socket.on(`${user_id} order state message`, (msg) => {
+  //     console.log(msg);
+  //     setM(msg); // Update m with the received msg
+  //   });
+  // }, [userId]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/restaurants/all?location=${selectedLocation}`
+        );
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const responseData = await response.text();
+        console.log("Response Data:", responseData);
+
+        const data = JSON.parse(responseData);
+
+        // Check if data is an object
+        if (typeof data === "object" && data !== null) {
+          // Transform object into the desired format
+          const transformedData = Object.keys(data).reduce((acc, key) => {
+            const intKey = parseInt(key, 10); 
+            const defaultValue = {
+              name: "麥當勞",
+              // image: "https://yt3.googleusercontent.com/ytc/APkrFKZi46pled4Gcj8WhRnYE1vO9Py1S-hDB1ntiybvCQ=s900-c-k-c0x00ffffff-no-rj",
+              prepare_time: 20,
+              evaluate: 5,
+              service: "內用|外帶",
+            };
+            return {
+              ...acc,
+              [intKey]: {
+                id: data[key].id,
+                name: data[key].name || defaultValue.name,
+                image: data[key].image,
+                prepare_time: data[key].prepare_time || defaultValue.prepare_time,
+                evaluate: data[key].evaluate || defaultValue.evaluate,
+                service: data[key].service || defaultValue.service,
+              },
+            };
+          }, {});
+
+          // Assuming you want to keep the object structure with integer keys
+          setRestaurants(transformedData);
+          setLoad(true);
+        } else {
+          // Handle the case when data is not in the expected format
+          throw new Error("Data is not in the expected format");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setError("Error fetching data");
+      }
+    };
+
+    fetchData();
+  }, [selectedLocation]);
+
+  //
+  // let restaurant_information = {
+  //   //全類別
+  //   1: {
+  //     id: 1,
+  //     name: "麥當勞",
+  //     image:
+  //       "https://yt3.googleusercontent.com/ytc/APkrFKZi46pled4Gcj8WhRnYE1vO9Py1S-hDB1ntiybvCQ=s900-c-k-c0x00ffffff-no-rj",
+  //     prepare_time: 20,
+  //     evaluate: 5,
+  //     service: "內用|外帶",
+  //   },
+  //   2: {
+  //     id: 2,
+  //     name: "麥當勞2",
+  //     image:
+  //       "https://yt3.googleusercontent.com/ytc/APkrFKZi46pled4Gcj8WhRnYE1vO9Py1S-hDB1ntiybvCQ=s900-c-k-c0x00ffffff-no-rj",
+  //     prepare_time: 20,
+  //     evaluate: 5,
+  //     service: "內用|外帶",
+  //   },
+  //   3: {
+  //     id: 3,
+  //     name: "麥當勞3",
+  //     image:
+  //       "https://yt3.googleusercontent.com/ytc/APkrFKZi46pled4Gcj8WhRnYE1vO9Py1S-hDB1ntiybvCQ=s900-c-k-c0x00ffffff-no-rj",
+  //     prepare_time: 20,
+  //     evaluate: 5,
+  //     service: "內用|外帶",
+  //   },
+  //   4: {
+  //     id: 4,
+  //     name: "麥當勞4",
+  //     image:
+  //       "https://yt3.googleusercontent.com/ytc/APkrFKZi46pled4Gcj8WhRnYE1vO9Py1S-hDB1ntiybvCQ=s900-c-k-c0x00ffffff-no-rj",
+  //     prepare_time: 20,
+  //     evaluate: 5,
+  //     service: "內用|外帶",
+  //   },
+  // };
+  console.log("data:", restaurant_information);
+  const [cabinet_information, setCabinet] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/restaurants/news?location=${selectedLocation}`);
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const responseData = await response.text();
+        console.log('Response Data:', responseData);
+
+        const data = JSON.parse(responseData);
+        setCabinet(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setError('Error fetching data');
+      }
+    };
+
+    fetchData();
+  }, [selectedLocation]);
+
+  // let cabinet_information = {
+  //   1: {
+  //     isBetweenStartEndTime: true,
+  //     id: 5,
+  //     name: "蘿拉點心舖1",
+  //     image:
+  //       "https://yt3.googleusercontent.com/ytc/APkrFKZi46pled4Gcj8WhRnYE1vO9Py1S-hDB1ntiybvCQ=s900-c-k-c0x00ffffff-no-rj",
+  //     service: "內用|外帶",
+  //     evaluate: 5,
+  //     prepare_time: 20,
+  //     LatestNews: "情人節預購開始了",
+  //   },
+  //   2: {
+  //     isBetweenStartEndTime: false,
+  //     id: 6,
+  //     name: "蘿拉點心舖2",
+  //     image:
+  //       "https://yt3.googleusercontent.com/ytc/APkrFKZi46pled4Gcj8WhRnYE1vO9Py1S-hDB1ntiybvCQ=s900-c-k-c0x00ffffff-no-rj",
+  //     LatestNews: "情人節預購開始了",
+  //   },
+  // };
+
+  const handleClassChange = (selectedValue) => {
+    setSelectedClass(selectedValue);
+  };
+
+  const handleLocationChange = (selectedValue) => {
+    setSelectedLocation(selectedValue);
+  };
+
+  return (
+    <div
+      className="d-flex justify-content-center vh-100"
+      style={{ backgroundColor: "#FFFEFD" }}
+    >
+       {<Notify />}
+      <div>
+        <div className="mb-3 p-3">
+          <ControllableStates onValueChange={handleLocationChange} />
+          {console.log(selectedLocation)}
+        </div>
+        <div className="mb-3 p-3">
+          <h2 style={{ marginLeft: "5px" }}>
+            <strong>想要吃什麼？</strong>
+            <img
+              src={image}
+              alt="main"
+              style={{
+                width: "50%",
+                position: "relative",
+                left: "30px",
+                bottom: "30px",
+              }}
+            />
+          </h2>
+          <div>
+            <div
+              style={{
+                width: "100%",
+                overflowX: "auto",
+                margin: "0 auto",
+                position: "absolute",
+                top: "165px",
+                right: "0px",
+              }}
+            >
+              <CabinetDisplay cabinetInformation={cabinet_information} />
+            </div>
+          </div>
+          <p style={{ marginTop: "85px" }}></p>
+        </div>
+        <div style={{ marginBottom: "10px", width: "100%" }}>
+          <Selector onValueChange={handleClassChange} />
+          {console.log(selectedClass)}
+        </div>
+        { load === true && (
+        <div className="mb-3 p-3">
+          <Search
+            restaurantInformation={restaurant_information}
+            onRestaurantClick={(key) =>
+              console.log(`Clicked on restaurant with key: ${key}`)
+            }
+            setSearchResults={setSearchResults} // Pass setSearchResults function to Search component
+          />
+        </div>)}
+        <div style={{ paddingBottom: "30px" }}>
+          {/* Display search results */}
+          {searchResults.length >= 0 && (
+            <div>
+              {searchResults.map((key) => (
+                <Link
+                  to={`/${userId}/restaurant/${restaurant_information[key].id}`}
+                  key={key}
+                  className="mb-3"
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  <div
+                    className="mb-3"
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      borderRadius: "15px",
+                      border: "2px solid #F0F0F0",
+                    }}
+                  >
+                    <div style={{ flex: "0 0 60%" }}>
+                      <img
+                        src={restaurant_information[key].image}
+                        alt={restaurant_information[key].name}
+                        style={{
+                          height: "100%",
+                          width: "100%",
+                          borderRadius: "15px 0px 0px 15px",
+                        }}
+                      />
+                    </div>
+                    <div
+                      style={{
+                        flex: "0 0 40%",
+                        alignItems: "center",
+                        paddingLeft: "1rem",
+                        borderRadius: "0px 15px 15px px",
+                      }}
+                    >
+                      <h4>
+                        <strong>{restaurant_information[key].name}</strong>
+                      </h4>
+                      <div>
+                        {restaurant_information[key].evaluate}{" "}
+                        <StarIcon
+                          style={{ fontSize: "20px", color: "#F4B63D" }}
+                        />
+                      </div>
+                      <div>{restaurant_information[key].service}</div>
+                      <div>
+                        {restaurant_information[key].prepare_time}~
+                        {restaurant_information[key].prepare_time + 10} min
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+      
+      <div style={{ position: "fixed", bottom: 0, width: "100%" }}>
+        <Navbar />
+      </div>
+    </div>
+  );
+}
+
+export default Main;
---
+++ frontend_user/src/order process/OrderState.js
@@ -0,0 +1,146 @@
+import React, { useState, useEffect } from "react";
+import BackButton from "../component/return";
+import Timeline from "../component/timeline-component";
+import imagepng from "../image/order_state.png";
+import { useParams, useNavigate } from "react-router-dom";
+import defaultImage from "../image/defaultMenu.png";
+
+function OrderState() {
+  const { userId, id, order_id } = useParams();
+  const navigate = useNavigate();
+
+  const [order_state, setOrder] = useState({});
+  const [error, setError] = useState(null);
+  console.log(error);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/orders/${order_id}/orderstate`
+        );
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const responseData = await response.json();
+        console.log("Response Data:", responseData);
+
+        setOrder(responseData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setError("Error fetching data");
+      }
+    };
+
+    fetchData();
+  }, [order_id]);
+
+  // let order_state = {
+  //   id: 1,
+  //   name: "麥當勞",
+  //   location: "台積總部及晶圓十二A廠",
+  //   time: "12:30",
+  //   accept: true,
+  //   process: false,
+  //   meals: {
+  //     1: {
+  //       name: "牛肉麵",
+  //       image:
+  //         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhG_cZjxXIlwfsjseD7-LMSMzWI7txguoSLjCbwM85&s",
+  //       quantity: 10,
+  //       addition: "備註的事項",
+  //     },
+  //   },
+  //   addition: "備註的事項",
+  // };
+
+  if (order_state.process) {
+    navigate(`/${userId}/evaluation/${id}/${order_id}`);
+    return null;
+  }
+
+  // reject 結束
+
+  return (
+    <div>
+      <div
+        style={{
+          height: "70px",
+          background: "#35A996",
+          borderRadius: "0px 0px 30px 30px",
+        }}
+      >
+        <BackButton style={{ color: "white" }} />
+        <p
+          style={{
+            color: "white",
+            fontSize: "1.8rem",
+            paddingTop: "10px",
+            marginLeft: "60px",
+          }}
+        >
+          訂單狀況
+        </p>
+      </div>
+      <div style={{ marginTop: "20px", marginBottom: "40px" }}>
+        <div
+          style={{ marginTop: "10px", marginLeft: "20px", fontSize: "20px" }}
+        >
+          <strong>{order_state.name}</strong>
+          <span style={{ fontSize: "15px", marginLeft: "140px" }}>
+            {order_state.location}
+          </span>
+          <p style={{ fontSize: "15px" }}>{order_state.time}</p>
+        </div>
+      </div>
+      {Object.keys(order_state).length > 0 && (
+        <Timeline orderState={order_state} />
+      )}
+      <img src={imagepng} alt="圖片" style={{ width: "100%" }} />
+      <h2 style={{ marginLeft: "10px" }}>
+        <strong>餐點資訊</strong>
+      </h2>
+      {order_state.Meals &&
+        Object.keys(order_state.Meals).map((mealId) => {
+          const meal = order_state.Meals[mealId];
+          return (
+            <div
+              key={mealId}
+              style={{
+                display: "flex",
+                marginLeft: "20px",
+                borderBottom: "2px solid gray",
+              }}
+            >
+              <div style={{ flex: "1" }}>
+                <p>
+                  <strong>{meal.name}</strong>
+                </p>
+                <p style={{ marginTop: "-15px" }}>{meal.quantity}</p>
+                <p style={{ marginTop: "-15px", backgroundColor: "#f4f4f4" }}>
+                  備註： {meal.addition}
+                </p>
+              </div>
+              <div style={{ flex: "0 0 40%" }}>
+                <img
+                  src={meal.image || defaultImage}
+                  alt={`${meal.name}`}
+                  style={{ marginLeft: "40px",marginTop:"-20px", width: "60%", height: "auto" }}
+                />
+              </div>
+            </div>
+          );
+        })}
+
+      <h2 style={{ margin: "10px" }}>
+        <strong>訂單備註</strong>
+      </h2>
+      <p style={{ marginLeft: "10px", backgroundColor: "#f4f4f4" }}>
+        {order_state.addition}
+      </p>
+    </div>
+  );
+}
+
+export default OrderState;
