137bd1fe23a942f389a2468a41ab483253579ffa
Azure0413 <88129659+Azure0413@users.noreply.github.com>
2023-12-20 22:01:13 +0800

    Add files via upload

---
+++ frontend_user/src/Main.js
@@ -0,0 +1,272 @@
+import React, { useState, useEffect } from "react";
+import ControllableStates from "./component/location";
+import image from "./image/main.png";
+import Navbar from "./component/navbar-component";
+import StarIcon from "@mui/icons-material/Star";
+import { Link, useParams, useNavigate } from "react-router-dom";
+// import imagei from "./image/imagei.jpg";
+import Selector from "./component/class";
+import Search from "./component/search";
+import CabinetDisplay from "./component/cabinet-component";
+import Notify from "./component/messenge-component";
+import io from "socket.io-client";
+
+function Main() {
+  const [selectedClass, setSelectedClass] = useState("all");
+  const [selectedLocation, setSelectedLocation] = useState("晶圓二廠");
+  const [searchResults, setSearchResults] = useState([]);
+  const { userId } = useParams();
+  const [load, setLoad] = useState(false);
+  const [restaurant_information, setRestaurants] = useState({});
+  const [error, setError] = useState(null);
+  console.log(error);
+  const [mes, setMes] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user_id = userId;
+
+    const socket = io();
+
+    socket.on(`${user_id} order state message`, (msg) => {
+      console.log(msg);
+      setMes(msg); // Update m with the received msg
+    });
+  }, [userId]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let apiUrl;
+
+        if (selectedClass === "all") {
+          apiUrl = `http://localhost:3000/restaurants/all?location=${selectedLocation}`;
+        } else {
+          apiUrl = `http://localhost:3000/restaurants/suball?location=${selectedLocation}&category=${selectedClass}`;
+        }
+
+        const response = await fetch(apiUrl);
+        console.log(
+          "目前廠區",
+          encodeURIComponent(selectedLocation),
+          "respond：",
+          response
+        );
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const responseData = await response.text();
+        console.log("Response Data:", responseData);
+
+        const data = JSON.parse(responseData);
+
+        // Check if data is an object
+        if (typeof data === "object" && data !== null) {
+          // Transform object into the desired format
+          const transformedData = Object.keys(data).reduce((acc, key) => {
+            const intKey = parseInt(key, 10);
+            const defaultValue = {
+              name: "麥當勞",
+              // image: "https://yt3.googleusercontent.com/ytc/APkrFKZi46pled4Gcj8WhRnYE1vO9Py1S-hDB1ntiybvCQ=s900-c-k-c0x00ffffff-no-rj",
+              prepare_time: 20,
+              evaluate: 5,
+              service: "內用|外帶",
+            };
+            return {
+              ...acc,
+              [intKey]: {
+                id: data[key].id,
+                name: data[key].name || defaultValue.name,
+                image: data[key].image,
+                prepare_time:
+                  data[key].prepare_time || defaultValue.prepare_time,
+                evaluate: data[key].evaluate || defaultValue.evaluate,
+                service: data[key].service || defaultValue.service,
+              },
+            };
+          }, {});
+
+          // Assuming you want to keep the object structure with integer keys
+          setRestaurants(transformedData);
+          setLoad(true);
+        } else {
+          // Handle the case when data is not in the expected format
+          throw new Error("Data is not in the expected format");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setError("Error fetching data");
+      }
+    };
+
+    fetchData();
+  }, [selectedLocation,selectedClass]);
+
+  console.log("data:", restaurant_information);
+  const [cabinet_information, setCabinet] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/restaurants/news?location=${selectedLocation}`
+        );
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const responseData = await response.text();
+        console.log("Response Data:", responseData);
+
+        const data = JSON.parse(responseData);
+        setCabinet(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setError("Error fetching data");
+      }
+    };
+
+    fetchData();
+  }, [selectedLocation]);
+
+  const handleClassChange = (selectedValue) => {
+    setSelectedClass(selectedValue);
+  };
+
+  const handleLocationChange = (selectedValue) => {
+    setSelectedLocation(selectedValue);
+  };
+
+  if (mes && mes.receive_state && mes.receive_state === true) {
+    navigate(`/${userId}/evaluation/${mes.id}/${mes.order_id}`);
+    return null;
+  }
+
+  return (
+    <div
+      className="d-flex justify-content-center vh-100"
+      style={{ backgroundColor: "#FFFEFD" }}
+    >
+      {<Notify />}
+      <div>
+        <div className="mb-3 p-3">
+          <ControllableStates onValueChange={handleLocationChange} />
+          {console.log(selectedLocation)}
+        </div>
+        <div className="mb-3 p-3">
+          <h2 style={{ marginLeft: "5px" }}>
+            <strong>想要吃什麼？</strong>
+            <img
+              src={image}
+              alt="main"
+              style={{
+                width: "50%",
+                position: "relative",
+                left: "30px",
+                bottom: "30px",
+              }}
+            />
+          </h2>
+          <div>
+            <div
+              style={{
+                width: "100%",
+                overflowX: "auto",
+                margin: "0 auto",
+                position: "absolute",
+                top: "165px",
+                right: "0px",
+              }}
+            >
+              <CabinetDisplay cabinetInformation={cabinet_information} />
+            </div>
+          </div>
+          <p style={{ marginTop: "85px" }}></p>
+        </div>
+        <div style={{ marginBottom: "10px", width: "100%" }}>
+          <Selector onValueChange={handleClassChange} />
+          {console.log(selectedClass)}
+        </div>
+        {load === true && (
+          <div className="mb-3 p-3">
+            <Search
+              restaurantInformation={restaurant_information}
+              onRestaurantClick={(key) =>
+                console.log(`Clicked on restaurant with key: ${key}`)
+              }
+              setSearchResults={setSearchResults} // Pass setSearchResults function to Search component
+            />
+          </div>
+        )}
+        <div style={{ paddingBottom: "30px" }}>
+          {/* Display search results */}
+          {searchResults.length >= 0 && (
+            <div>
+              {searchResults.map((key) => (
+                <Link
+                  to={`/${userId}/restaurant/${restaurant_information[key].id}`}
+                  key={key}
+                  className="mb-3"
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  <div
+                    className="mb-3"
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      borderRadius: "15px",
+                      border: "2px solid #F0F0F0",
+                    }}
+                  >
+                    <div style={{ flex: "0 0 60%" }}>
+                      <img
+                        src={restaurant_information[key].image}
+                        alt={restaurant_information[key].name}
+                        style={{
+                          height: "100%",
+                          width: "100%",
+                          borderRadius: "15px 0px 0px 15px",
+                        }}
+                      />
+                    </div>
+                    <div
+                      style={{
+                        flex: "0 0 40%",
+                        alignItems: "center",
+                        paddingLeft: "1rem",
+                        borderRadius: "0px 15px 15px px",
+                      }}
+                    >
+                      <h4>
+                        <strong>{restaurant_information[key].name}</strong>
+                      </h4>
+                      <div>
+                        {restaurant_information[key].evaluate}{" "}
+                        <StarIcon
+                          style={{ fontSize: "20px", color: "#F4B63D" }}
+                        />
+                      </div>
+                      <div>{restaurant_information[key].service}</div>
+                      <div>
+                        {restaurant_information[key].prepare_time}~
+                        {restaurant_information[key].prepare_time + 10} min
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div style={{ position: "fixed", bottom: 0, width: "100%" }}>
+        <Navbar />
+      </div>
+    </div>
+  );
+}
+
+export default Main;
---
+++ frontend_user/src/order process/Shop.js
@@ -0,0 +1,304 @@
+// let item = {
+//   shop_id: 1,
+//   restaurant: "麥當勞",
+//   total: 300,
+//   items: {
+//     1: {
+//       name: "牛肉麵",
+//       price: 100,
+//       quantity: 1,
+//       image:
+//         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhG_cZjxXIlwfsjseD7-LMSMzWI7txguoSLjCbwM85&s",
+//     },
+//     2: {
+//       name: "牛肉麵2",
+//       price: 100,
+//       quantity: 2,
+//       image:
+//         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhG_cZjxXIlwfsjseD7-LMSMzWI7txguoSLjCbwM85&s",
+//     },
+//   },
+// };
+import React, { useState, useEffect, useId } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import BackButton from "../component/retrun-order";
+import Counter from "../component/plus_minus";
+import defaultImage from "../image/defaultMenu.png";
+import menupng from "../image/defaultImage.jpg";
+
+function Shop() {
+  const navigate = useNavigate();
+  const [counts, setCounts] = useState({});
+  const { userId } = useParams();
+  const { id } = useParams();
+  const [note, setNote] = useState("");
+  const [inputValues, setInputValues] = useState({});
+  const [item, setItem] = useState([]);
+  const [error, setError] = useState(null);
+  console.log(error);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/shopping/cartinfo/user/${userId}/shop/${id}`
+        );
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const responseData = await response.json();
+        console.log("Response Data:", responseData);
+
+        // Create the desired structure for the item state
+        setItem(responseData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setError("Error fetching data");
+      }
+    };
+
+    fetchData();
+  }, [userId, id]);
+
+  const handleCountChange = (itemId, newCount) => {
+    setCounts((prevCounts) => ({
+      ...prevCounts,
+      [itemId]: newCount,
+    }));
+  };
+
+  const handleNoteChange = (event) => {
+    // Update the note value in the state
+    setNote(event.target.value);
+  };
+
+  useEffect(() => {
+    // Initialize input values when item changes
+    const initialInputValues = {};
+    if (item && item.items) {
+      Object.keys(item.items).forEach((itemId) => {
+        initialInputValues[itemId] = item.items[itemId].addition || "";
+      });
+    }
+    setInputValues(initialInputValues);
+  }, [item]);
+
+  const handleInputChange = (itemId, e) => {
+    const value = e.target.value;
+    setInputValues((prevInputValues) => ({
+      ...prevInputValues,
+      [itemId]: value,
+    }));
+  };
+
+  let subtotals = [];
+  let total = 0;
+  // Calculate subtotal for each item
+  if (item && item.items) {
+    subtotals = Object.keys(item.items).map((itemId) => {
+      const currentItem = item.items[itemId];
+      const count =
+        counts[itemId] !== undefined ? counts[itemId] : currentItem.quantity; // Use 0 if count is not set
+      return count * currentItem.price;
+    });
+
+    total = subtotals.reduce((acc, subtotal) => acc + subtotal, 0);
+  }
+
+  const handleClick = (shop_id) => {
+    navigate(`/${userId}/restaurant/${id}`);
+  };
+
+  const handleClick2 = async () => {
+    const data = {
+      shop_id: id,
+      user_id: userId,
+      items: Object.keys(item.items).reduce((acc, itemId) => {
+        const currentItem = item.items[itemId];
+  
+        const count =
+          counts[itemId] !== undefined ? counts[itemId] : currentItem.quantity;
+        const noteValue = inputValues[itemId] || currentItem.addition;
+  
+        acc[itemId] = {
+          name: currentItem.name,
+          price: currentItem.price,
+          quantity: count,
+          addition: noteValue,
+        };
+        return acc;
+      }, {}),
+      addition: note,
+    };
+    try {
+      const response = await fetch(
+        "http://localhost:3000/shopping/update/items/note",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+
+      if (response.ok) {
+        console.log("Data posted successfully");
+        console.log(data);
+      } else {
+        console.error(`Error: ${response.status}, ${await response.text()}`);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+
+    navigate(`/${userId}/restaurant/${id}/checkout`);
+  };
+
+  return (
+    <div>
+      <div
+        style={{ display: "flex", flexDirection: "column", height: "100vh" }}
+      >
+        <img
+          src={menupng}
+          alt="menu"
+          style={{ width: "100%", height: "10%", objectFit: "cover" }}
+        />
+        <div
+          style={{
+            borderRight: "2px solid gray",
+            borderLeft: "2px solid gray",
+            borderTop: "2px solid gray",
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            position: "relative",
+          }}
+        >
+          <BackButton />
+          <h2
+            style={{ marginTop: "50px", marginLeft: "20px", fontSize: "40px" }}
+          >
+            <strong>{item.restaurant}</strong>
+          </h2>
+          {item &&
+            item.items &&
+            Object.keys(item.items).map((itemId) => {
+              const currentItem = item.items[itemId];
+              return (
+                <div
+                  key={itemId}
+                  style={{
+                    paddingLeft: "20px",
+                    paddingBottom: "10px",
+                    borderBottom: "2px solid gray",
+                  }}
+                >
+                  <span style={{ fontSize: "25px" }}>
+                    <strong>{currentItem.name}</strong>
+                  </span>
+                  <span style={{ marginLeft: "170px" }}>
+                    {" "}
+                    <img
+                      src={currentItem.image || defaultImage}
+                      style={{ width: "25%" }}
+                      alt="圖片"
+                    />{" "}
+                  </span>
+                  <p></p>
+                  <Counter
+                    initialValue={parseInt(currentItem.quantity, 10)}
+                    onCountChange={(newCount) =>
+                      handleCountChange(itemId, newCount)
+                    }
+                  />
+                  <p style={{ marginLeft: "300px" }}>
+                    ${" "}
+                    {counts[itemId] !== undefined
+                      ? currentItem.price * counts[itemId]
+                      : currentItem.price * currentItem.quantity}
+                  </p>
+                  <input
+                    type="text"
+                    style={{
+                      width: "90%",
+                      border: "none",
+                      backgroundColor: "#D3D3D3",
+                    }}
+                    defaultValue={currentItem.addition}
+                    value={inputValues[itemId] || currentItem.addition}
+                    onChange={(e) => handleInputChange(itemId, e)}
+                  />
+                </div>
+              );
+            })}
+
+          <strong>
+            <p style={{ marginLeft: "20px" }}>
+              小計<span style={{ marginLeft: "280px" }}>{total}</span>
+            </p>
+          </strong>
+          <p style={{ paddingLeft: "20px", fontSize: "1.2em" }}>
+            <strong>新增備註</strong>
+          </p>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <input
+              className="rounded"
+              placeholder="備註"
+              type="text"
+              value={note} // Set the value of the input from state
+              onChange={handleNoteChange} // Handle changes to the input
+              style={{
+                border: "none",
+                width: "90%",
+                height: "100px",
+                fontSize: "16px",
+                backgroundColor: "#D3D3D3",
+              }}
+            />
+          </div>
+          <p></p>
+          <button
+            id="continue_btn"
+            className="rounded"
+            onClick={() => handleClick(item.shop_id)}
+            style={{
+              border: "none",
+              height: "30px",
+              width: "90%",
+              marginLeft: "20px",
+              backgroundColor: "#F4B63D",
+            }}
+          >
+            <strong>繼續點餐</strong>
+          </button>
+          <p></p>
+          <button
+            id="checkout_btn"
+            className="rounded"
+            onClick={handleClick2}
+            style={{
+              border: "none",
+              height: "30px",
+              width: "90%",
+              marginLeft: "20px",
+              backgroundColor: "#F4B63D",
+            }}
+          >
+            <strong>前往結帳</strong>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Shop;
