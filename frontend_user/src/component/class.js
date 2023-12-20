b6eebd1dea963adc80a55d458495788bb5db02c5
Azure0413 <88129659+Azure0413@users.noreply.github.com>
2023-12-21 04:29:40 +0800

    Add files via upload

---
+++ frontend_user/src/component/class.js
@@ -0,0 +1,64 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import OutlinedInput from "@mui/material/OutlinedInput";
+
+export default function Selector({ onValueChange }) {
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event) => {
+    const selectedValue = event.target.value;
+    setAge(selectedValue);
+    onValueChange(selectedValue); // Pass the selected value to the parent component
+  };
+
+  let region = {
+    location: [
+      "先進封測三廠",
+      "台積總部及晶圓十二A廠",
+      "研發中心及晶圓十二B廠",
+    ],
+    class: [
+      "all",
+      "健康",
+      "甜點",
+      "飲品",
+      "中式",
+      "日式",
+      "韓式",
+      "義式",
+      "美式",
+      "泰式",
+    ],
+  };
+
+  return (
+    <Box className="mx-auto" sx={{ width: "90%" }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label"></InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="location"
+          onChange={handleChange}
+          input={<OutlinedInput />}
+          sx={{ borderRadius: "20px", height: "30px", border: "2px solid #F4B63D" }}
+          displayEmpty
+        >
+          <MenuItem value="" disabled>
+            <em>類別</em>
+          </MenuItem>
+          {region.class.map((item, index) => (
+            <MenuItem key={index} value={region.class[index]}>
+              {region.class[index]}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
